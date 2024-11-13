2b2b5dd827246563912e225f2d8b07d9acebd0df
tsw96d <tsw96d@compute-17-35.mill>
2024-11-13 13:26:13 -0600

    update

--- test.py
+++ test.py
@@ -2,8 +2,10 @@ from ultralytics import YOLO
 
 # Initialize your custom model
 model = YOLO("./ultralytics/cfg/models/v8/yolov8_MobileNetv4.yaml")
+# model = YOLO("./ultralytics/cfg/models/11/yolo11.yaml")
 
-# Train the model using COCO128
-model.train(data="coco.yaml", epochs=10, imgsz=640, batch=16)
+results = model.train(data="/home/tsw96d/YOLOComparision/FLIR_v2.yaml", epochs=200, batch=64, project="../outputs/training", name = "MobleNet4") 
 
-# model.val(data="coco128.yaml")
+# results = model.val(data="/home/tsw96d/YOLOComparision/FLIR_v2.yaml", epochs=200, batch=64, project="../outputs/validation", name = "MobleNet4")
+
+# success = model.export(format="onnx")
