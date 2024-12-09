b489c7b7f9097059ade54fc53cf198a90ccf7d13
tsw96d <tsw96d@compute-17-27.mill>
2024-12-09 01:19:05 -0600

    update

--- train.py
+++ train.py
@@ -5,14 +5,14 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     model = YOLO("yolov8-hgnet.yaml")
    # model.load('yolov8n.pt') 
-    model.train(data=r'ultralytics/cfg/datasets/FLIR_v2.yaml',
-                cache=False,
+    model.train(data=r'/home/tsw96d/YOLOComparision/FLIR_v2.yaml',
+                cache=True,
                 imgsz=640,
-                epochs=2,
-                batch=16,
+                epochs=200,
+                batch=32,
                 close_mosaic=0,
-                workers=0,
+                workers=8,
                 device= None,
                 optimizer='SGD', # using SGD
-                amp=False,# close amp
+                amp=True,# close amp
                 )
